caae2a9ad73fcda09443bc178c26194e6d9dc27f
Anton Veretennikov <superiorboroda@gmail.com>
2017-07-31 13:07:57 +0300

    Add All commit

---
+++ TypeScriptLearning/gulpfile.js
@@ -0,0 +1,26 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var del = require('del');
+
+var paths = {
+	scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map'],
+	wwwroot: 'wwwroot/scripts'
+};
+
+gulp.task('clean', function () {
+	return del(['wwwroot/scripts/**/*']);
+});
+
+
+gulp.task('clean', function () {
+	return del(['wwwroot/scripts/**/*']);
+});
+
+gulp.task('default', function () {
+	gulp.src(paths.scripts).pipe(gulp.dest(paths.wwwroot))
+});
