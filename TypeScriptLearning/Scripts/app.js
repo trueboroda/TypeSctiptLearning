e23df5d98fefde795b5c3a4cad6dc93f2457b1b9
Anton Veretennikov <superiorboroda@gmail.com>
2017-08-08 18:22:03 +0300

    Интерфейсы функций и массивов. Гибридные интерфейсы

--- TypeScriptLearning/Scripts/app.js
+++ TypeScriptLearning/Scripts/app.js
@@ -30,6 +30,33 @@ var auto = new Car();
 auto.speed = 60;
 auto.fill();
 auto.move();
+var simpleBuilder = function (name, surname) {
+    return "Mr." + name + " " + surname;
+};
+var fullName = simpleBuilder("Tony", "Montana");
+console.log(fullName);
+var phones;
+phones = ["iPhone 7", "HTC 10", "HP Elite x3"];
+var myPhone = phones[0];
+console.log(myPhone);
+var colors = {};
+colors["red"] = "#ff0000";
+colors["green"] = "#00ff00";
+colors["blue"] = "#0000ff";
+console.log(colors["red"]);
+function personBuilder() {
+    var person = function (name, surname) {
+        person.fullName = name + " " + surname;
+    };
+    person.authenticate = function () {
+        console.log(person.fullName + " входит в систему с паролем " + person.password);
+    };
+    return person;
+}
+var tom = personBuilder();
+tom("Tom", "Simpson");
+tom.password = "qwerty";
+tom.authenticate();
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
--- TypeScriptLearning/wwwroot/js/app.ts
+++ TypeScriptLearning/wwwroot/js/app.ts
@@ -57,6 +57,62 @@ auto.speed = 60;
 auto.fill();
 auto.move();
 
+
+interface FullNameBuilder {
+	(name: string, surname: string): string;
+}
+
+let simpleBuilder: FullNameBuilder = function (name: string, surname: string): string {
+	return "Mr." + name + " " + surname;
+}
+
+let fullName = simpleBuilder("Tony", "Montana");
+console.log(fullName);
+
+interface StringArray {
+	    [index: number]: string;
+}
+
+let phones: StringArray;
+phones = ["iPhone 7", "HTC 10", "HP Elite x3"];
+
+let myPhone: string = phones[0];
+console.log(myPhone);
+
+interface Dictionary {
+	[index: string]: string;
+}
+
+var colors: Dictionary = {};
+colors["red"] = "#ff0000";
+colors["green"] = "#00ff00";
+colors["blue"] = "#0000ff";
+
+console.log(colors["red"]);
+
+interface PersonInfo {
+	(name: string, surname: string): void;
+	fullName: string;
+	password: string;
+	authenticate(): void;
+}
+
+function personBuilder(): PersonInfo {
+
+	let person = <PersonInfo>function (name: string, surname: string): void {
+		person.fullName = name + " " + surname;
+	};
+	person.authenticate = function () {
+		console.log(person.fullName + " входит в систему с паролем " + person.password);
+	};
+	return person;
+}
+
+let tom = personBuilder();
+tom("Tom", "Simpson");
+tom.password = "qwerty";
+tom.authenticate();
+
 window.onload = () => {
 	var el = document.getElementById('content');
 	var greeter = new Greeter(el);
